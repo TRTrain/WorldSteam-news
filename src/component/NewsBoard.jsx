49ce0a38a32aad13e937489f63fc90f30ecf9b3f
TRTrain <piyad8236@gmail.com>
2023-11-08 00:36:05 +0700

    news website

---
+++ src/component/NewsBoard.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import NewsItem from "./NewsItem";
+
+const NewsBoard = ({category}) => {
+
+    const [articles,setArticles] = useState([]);
+
+    useEffect(()=>{
+        let url =  `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=c72cffce7ea6413ab443381b2fa1ad1b`;
+       fetch(url).then(Response=> Response.json()).then(data=> setArticles(data.articles));
+    },[category])
+
+  return (
+    <div>
+
+      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+        {articles.map((news,index)=>{
+            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+        })}
+    </div>
+  )
+}
+
+export default NewsBoard
+
+             
